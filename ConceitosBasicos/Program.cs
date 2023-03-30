23adfb5547ec9d20f790d2b47acafb0fc943c75c
Aula <Aula@CANP-LABTIC12>
2023-03-30 15:39:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Hello, World!");
+string nome = "seu nome";
+Console.WriteLine("Seja bemvindo(a)" + nome);
+string cidade = "Pinheiral";
+Console.WriteLine($"Eu gosto de {cidade}");
+
+int idade = 16;
+Console.WriteLine($"idade é {idade}");
+
+byte valor = 255;
+valor += 1; // valor = valor + 1;
+Console.WriteLine($"valor: {valor}");
+
+float salario = 5000.20m;
+// como seria a declaração mais adequada para variaveis que armazenam altura,peso e altura em nanometros;
+
+float altura = 1.64f;
+Console.WriteLine($"a altura é {altura}");
+float peso = 43.6f;
+Console.WriteLine($"o peso informado foi {peso}");
+float Pesoemnanometro =16101912.06;
+Console.WriteLine($"se vc pudesse ser o homem formiga, teria a altura {Pesoemnanometro}");
+
+
